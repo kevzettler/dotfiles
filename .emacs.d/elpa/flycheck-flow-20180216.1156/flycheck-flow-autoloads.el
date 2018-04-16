45047b386b40dd65b03af4fbfc3844316c6ad994
kevzettler <kevzettler@gmail.com>
2018-04-16 12:31:12 -0700

    add all missing hidden dependencies

---
+++ .emacs.d/elpa/flycheck-flow-20180216.1156/flycheck-flow-autoloads.el
@@ -0,0 +1,15 @@
+;;; flycheck-flow-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-flow.el") (23243 46122 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-flow-autoloads.el ends here
---
+++ .emacs.d/elpa/flycheck-flow-20180216.1156/flycheck-flow-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-flow" "20180216.1156" "Support Flow in flycheck" '((flycheck "0.18") (json "1.4")) :commit "8173accf0e389f7ba0936d399da84839a7f887af")
