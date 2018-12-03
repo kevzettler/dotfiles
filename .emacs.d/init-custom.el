8fc2a13e5b126eb4ad5afd651e3af4df7e0494ee
kevzettler <kevzettler@gmail.com>
2018-12-02 17:13:45 -0800

    add .gitignore file and add eshell history to it

--- .emacs.d/init-custom.el
+++ .emacs.d/init-custom.el
@@ -6,43 +6,43 @@
  '(company-tooltip-idle-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(epg-gpg-program "/usr/local/opt/gnupg@2.1/bin/gpg2")
  '(flycheck-javascript-flow-args nil)
  '(js-indent-level 2)
  '(package-selected-packages
    (quote
-    (company-flow jade-mode pug-mode counsel-projectile counsel org-brain merlin tuareg pos-tip company-tern tern company-web
-                  (js2-mode
-                   (web-mode
-                    (glsl-mode
-                     (iedit
-                      (company
+    (org-journal company-flow jade-mode pug-mode counsel-projectile counsel org-brain merlin tuareg pos-tip company-tern tern company-web
+                 (js2-mode
+                  (web-mode
+                   (glsl-mode
+                    (iedit
+                     (company
+                      (sass-mode
                        (sass-mode
                         (sass-mode
-                         (sass-mode
-                          ((js2-mode
-                            (org-preview-html
-                             (typescript-mode
-                              (circe
-                               (expand-region
-                                (markdown-mode
-                                 (ivy
-                                  (helm
-                                   (js2-mode
-                                    (flycheck
+                         ((js2-mode
+                           (org-preview-html
+                            (typescript-mode
+                             (circe
+                              (expand-region
+                               (markdown-mode
+                                (ivy
+                                 (helm
+                                  (js2-mode
+                                   (flycheck
+                                    (flymake-jshint
                                      (flymake-jshint
-                                      (flymake-jshint
-                                       (use-package
-                                         (org-gcal
+                                      (use-package
+                                        (org-gcal
+                                         (yaml-mode
                                           (yaml-mode
-                                           (yaml-mode
-                                            (multiple-cursors
-                                             (web-mode
-                                              (exec-path-from-shell
-                                               (auto-complete
-                                                (solarized-theme auto-complete
-                                                                 (solarized-theme tern nvm exec-path-from-shell yaml-mode web-mode sass-mode php-mode org multiple-cursors markdown-mode json-mode jade-mode furl flycheck editorconfig color-theme-solarized auto-complete ac-js2))))))))))))))))))))))))))))))))))
+                                           (multiple-cursors
+                                            (web-mode
+                                             (exec-path-from-shell
+                                              (auto-complete
+                                               (solarized-theme auto-complete
+                                                                (solarized-theme tern nvm exec-path-from-shell yaml-mode web-mode sass-mode php-mode org multiple-cursors markdown-mode json-mode jade-mode furl flycheck editorconfig color-theme-solarized auto-complete ac-js2))))))))))))))))))))))))))))))))))
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 587))
